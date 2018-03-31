0fab268899b275a0a68b030bea7e3335ae62af42
Kashyap Murthy <kashyap.skash@gmail.com>
2018-04-01 03:47:36 +0530

    Adding Routers to main App
    -- Making use of BrowserRender, Route and Switch from react-render-dom

--- src/index.js
+++ src/index.js
@@ -3,17 +3,40 @@ import ReactDOM from 'react-dom';
 import Login from './Login/login';
 import "./Login/login.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route, Router, BrowserRouter} from 'react-router';
-import routes from './routes';
-import {Provider} from 'react-redux';
-import { createStore } from 'redux'; 
-import TodoReducers from './reducers';
-import Order from './Order/order';
+// import {Route, Router, BrowserRouter} from 'react-router';
+// import routes from './routes';
+// import {Provider} from 'react-redux';
+// import { createStore } from 'redux'; 
+// import TodoReducers from './reducers';
 
-let store = createStore(TodoReducers); 
+import Order from './Order/order';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// let store = createStore(TodoReducers); 
 
 const app = document.getElementById("root");
 
+const App = () => (
+  <div>
+    <Main />
+  </div>
+);
+
+const Main = () => (
+    <Switch>
+      <Route exact path='/' component={Login}/>
+      <Route path='/orders' component={OrdersRoute}/>
+    </Switch>
+);
+
+const OrdersRoute = () => (	
+	<Switch>
+    	<Route exact path='/orders' component={Order}/>
+  	</Switch>
+);
+
 ReactDOM.render( 
-  <Order></Order>, app
+	<Router>
+	<App />
+	</Router>, app
 );
